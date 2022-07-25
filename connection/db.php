928dfe7e6e3919d1bfa955b47b550a13ae577cbd
Novan Rohman Nur Khoir <novanrohman@gmail.com>
2022-07-25 14:07:33 +0700

    update login

---
+++ connection/db.php
@@ -0,0 +1,13 @@
+<?php
+$server = "127.0.0.1";
+$user = "root";
+$pass = "";
+$database="db_kp";
+
+$conn = new mysqli($server, $user, $pass, $database);
+
+// if($conn -> connect_error) {
+//   die("Connection error" . $conn->connection_error);
+// }
+// echo "Connected";
+?>
