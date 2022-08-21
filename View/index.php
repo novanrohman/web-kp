1cd3749ef1cfd312fc6bc9ce94cd74fa554a30f6
Novan Rohman Nur Khoir <novanrohman@gmail.com>
2022-08-21 20:20:15 +0700

    username

--- View/index.php
+++ View/index.php
@@ -190,7 +190,7 @@ if (!isset($_SESSION['username'])) {
               <img src="../AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-              <a href="#" class="d-block">Alexander Pierce</a>
+              <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
             </div>
           </div>
   
