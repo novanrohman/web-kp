6ce908fccd3140301aed78d6007975affff7b70a
Novan Rohman Nur Khoir <34656026+novanrohman@users.noreply.github.com>
2022-08-21 21:17:41 +0700

    Novan (#2)
    * username
    
    * backup database

--- View/index.php
+++ View/index.php
@@ -200,7 +200,7 @@ $db = new database();
               <img src="../AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-              <a href="#" class="d-block">Alexander Pierce</a>
+              <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
             </div>
           </div>
   
