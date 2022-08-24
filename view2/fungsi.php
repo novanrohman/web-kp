9805f598c788dd1152526db4aea6a04af4bfe32a
HAMSYAFF <angga10nugraha10@gmail.com>
2022-08-24 08:23:59 +0700

    menambhakan daftarkp

---
+++ view2/fungsi.php
@@ -0,0 +1,55 @@
+<?php 
+    $conn = mysqli_connect("localhost","root","","db_kp");
+    function query($query){
+        global $conn;
+        $result = mysqli_query($conn, $query);
+        $row =[];
+        while ($rows = mysqli_fetch_assoc($result)){
+            $row[] = $rows;
+        }
+        return $row;
+    }
+    function daftar_kp($data){
+        global $conn;
+        $tempat_kp = htmlspecialchars($data["tempat_kp"]);
+        $alamat_kp = htmlspecialchars($data["alamat_kp"]);
+        $tanggal_mulai = htmlspecialchars($data["tanggal_mulai"]);
+        $tanggal_selesai = htmlspecialchars($data["tanggal_selesai"]);
+        $anggota_kelompok = ($data["anggota_kelompok"]);
+        $dosen_id =  ($data["dosen_id"]);
+        $perusahaan_id =  ($data["perusahaan_id"]);
+        $proposal = upload_prop();
+        if(!$proposal){
+            return false;
+        }
+        $perusahaan_id =  ($data["perusahaan_id"]);
+        $query = "INSERT INTO pendaftaran_kp VALUES ('','$tempat_kp','$alamat_kp','$tanggal_mulai','$tanggal_selesai','$proposal','$anggota_kelompok','$dosen_id','$perusahaan_id')";
+        mysqli_query($conn,$query);
+        return mysqli_affected_rows($conn);
+    }
+    function upload_prop(){
+    $file = $_FILES['proposal']['name'];
+    $lokasi = $_FILES['proposal']['tmp_name'];
+    $ekstensiFileValid = ['pdf'];
+    $error = $_FILES['proposal']['error'];
+    $ekstensiFile = explode('.', $file);
+    $ekstensiFile = strtolower(end($ekstensiFile));
+    if ($error === 4) {
+         echo"
+        <script>
+        alert('Masukan File Terlebih dahulu');
+        </script>
+        ";
+    }
+    if( !in_array($ekstensiFile, $ekstensiFileValid) ){
+        echo"
+        <script>
+        alert('Pastikan File Berektensi PDF');
+        </script>
+        ";
+        return false;
+    }
+    move_uploaded_file($lokasi, "file/".$file);
+    return $file;
+    }
+?>
--- view2/pendaftarankp.php
+++ view2/pendaftarankp.php
@@ -1,96 +1,121 @@
+<?php
+require "fungsi.php";
+$mahasiswa = query("SELECT * FROM anggota_kelompok");
+$dosen = query("SELECT * FROM dosen");
+$perusahaan = query("SELECT * FROM perusahaan");
+if (isset($_POST["submit"])) {
+  if (daftar_kp($_POST) > 0) {
+    echo "<script>
+          alert ('data berhasil ditambah');
+          document.location .href = 'kp.php'
+      </script>";
+  } else {
+    echo "<script>
+          alert ('data gagal ditambah');
+          document.location.href = 'kp.php'
+      </script>";
+  }
+}
+
+
+?>
+
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content">
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-            <div class="card">
-              <div class="card-body">
-              <form class="form-horizontal" method="post">
-                      <div class="form-group row">
-                        <label for="Tempat" class="col-sm-2 col-form-label">Tempat</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="Tempat" placeholder="Tempat KP">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="Alamat" class="col-sm-2 col-form-label">Alamat</label>
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="Alamat" placeholder="Alamat">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="Tanggal Mulai" class="col-sm-2 col-form-label">Tanggal Mulai</label>
-                        <div class="col-sm-10">
-                          <input type="date" class="form-control" id="Tanggal Mulai" placeholder="Tanggal Mulai">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="Tanggal Selesai" class="col-sm-2 col-form-label">Tanggal Selesai</label>
-                        <div class="col-sm-10">
-                          <input type="date" class="form-control" id="Tanggal Selesai" placeholder="Tanggal Selesai">
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <label for="Proposal" class="col-sm-2 col-form-label">Proposal</label>
-                        <div class="col-sm-10">
-                          <input type="file" class="form-control" id="Proposal" placeholder="Proposal">
-                        </div>
-                      </div>   
-                      <div class="form-group row">
-                        <label for="Anggota Mahasiswa" class="col-sm-2 col-form-label">Anggota Mahasiswa</label>
-                        <div class="col-sm-10">
-                          <select class="form-control">
-                            <option value="">Pilih Mahasiswa</option>
-                            <option value="">Mahasiswa</option>
-                            <option value="">Mahasiswi</option>
-                          </select>
-                        </div>
-                      </div>   
-                      <div class="form-group row">
-                        <label for="Dosen Pengampu" class="col-sm-2 col-form-label">Dosen Pengampu</label>
-                        <div class="col-sm-10">
-                          <select class="form-control">
-                            <option value="">Pilih Dosen</option>
-                            <option value="">Pak Dim</option>
-                            <option value="">Bu Dianni</option>
-                          </select>
-                        </div>
-                      </div> 
-                      <div class="form-group row">
-                        <label for="Perusahaan" class="col-sm-2 col-form-label">Perusahaan</label>
-                        <div class="col-sm-10">
-                          <select class="form-control">
-                            <option value="">Pilih Perusahaan</option>
-                            <option value="">PT. Poliwangi Jaya</option>
-                            <option value="">PT. Poliwangi Merakyat</option>
-                          </select>
-                        </div>
-                      </div>                 
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <div class="checkbox">
-                            <label>
-                              <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                          <button type="submit" class="btn btn-danger" name="submit">Submit</button>
-                        </div>
-                      </div>
-                    </form>
+          <div class="card">
+            <div class="card-body">
+              <form class="form-horizontal" method="post" enctype="multipart/form-data">
+                <div class="form-group row">
+                  <label for="Tempat" class="col-sm-2 col-form-label">Tempat</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="Tempat" placeholder="Tempat KP" name="tempat_kp">
                   </div>
-                  <!-- /.tab-pane -->
                 </div>
-                <!-- /.tab-content -->
-              </div><!-- /.card-body -->
+                <div class="form-group row">
+                  <label for="Alamat" class="col-sm-2 col-form-label">Alamat</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="Alamat" placeholder="Alamat" name="alamat_kp">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Tanggal Mulai" class="col-sm-2 col-form-label">Tanggal Mulai</label>
+                  <div class="col-sm-10">
+                    <input type="date" class="form-control" id="Tanggal Mulai" placeholder="Tanggal Mulai" name="tanggal_mulai">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Tanggal Selesai" class="col-sm-2 col-form-label">Tanggal Selesai</label>
+                  <div class="col-sm-10">
+                    <input type="date" class="form-control" id="Tanggal Selesai" placeholder="Tanggal Selesai" name="tanggal_selesai">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Proposal" class="col-sm-2 col-form-label">Proposal</label>
+                  <div class="col-sm-10">
+                    <input type="file" class="form-control" id="Proposal" placeholder="Proposal" name="proposal">
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Anggota Mahasiswa" class="col-sm-2 col-form-label">Anggota Mahasiswa</label>
+                  <div class="col-sm-10">
+                    <select class="form-control" name="anggota_kelompok">
+                      <option value="">Pilih Mahasiswa</option>
+                      <?php foreach ($mahasiswa as $mhs) : ?>
+                        <option value="<?= $mhs['id'] ?>"><?= $mhs['nama_anggota'] ?></option>
+                      <?php endforeach; ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Dosen Pengampu" class="col-sm-2 col-form-label">Dosen Pengampu</label>
+                  <div class="col-sm-10">
+                    <select class="form-control" name="dosen_id">
+                      <option value="">Pilih Dosen</option>
+                      <?php foreach ($dosen as $dns) : ?>
+                        <option value="<?= $dns['id'] ?>"><?= $dns['nama_dosen'] ?></option>
+                      <?php endforeach; ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <label for="Perusahaan" class="col-sm-2 col-form-label">Perusahaan</label>
+                  <div class="col-sm-10">
+                    <select class="form-control" name="perusahaan_id">
+                      <option value="">Pilih Perusahaan</option>
+                      <?php foreach ($perusahaan as $psh) : ?>
+                        <option value="<?= $psh['id'] ?>"><?= $psh['nama_perusahaan'] ?></option>
+                      <?php endforeach; ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="offset-sm-2 col-sm-10">
+                    <div class="checkbox">
+                      <label>
+                        <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="offset-sm-2 col-sm-10">
+                    <button type="submit" class="btn btn-danger" name="submit">Submit</button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <!-- /.card -->
+            <!-- /.tab-pane -->
           </div>
+          <!-- /.tab-content -->
+        </div><!-- /.card-body -->
       </div>
+      <!-- /.card -->
     </div>
-  </section>
+</div>
+</div>
+</section>
 </div>
