b1eff572c4b3dc913f353206ed4655d7cad913ca
HAMSYAFF <angga10nugraha10@gmail.com>
2022-08-31 05:40:11 +0700

    mengubah config oop

--- connection/dbconfig.php
+++ connection/dbconfig.php
@@ -1,30 +1,34 @@
-<?php 
- 
-	class Databases {
- 
- 
-		public $host = 'localhost';
-		public $name = 'root';
-		public $pass = '';
-		public $dbname = 'db_kp';
-		
-		public $mysqli;
- 
-		function __construct (){
- 
+<?php
+
+class Databases
+{
+
+
+	public $host = 'localhost';
+	public $name = 'root';
+	public $pass = '';
+	public $dbname = 'db_kp';
+
+	public $mysqli;
+
+	function __construct()
+	{
+
 		$this->mysqli = new mysqli($this->host, $this->name, $this->pass, $this->dbname);
- 
+
 		if ($this->mysqli->connect_errno) {
 			echo "DATABASE TIDAK ADA !  ";
 			exit;
 		}
- 
 	}
 
-    public function get_dataDosen (){
+	public function get_dataDosen()
+	{
+		global $conn;
 		$id = $_SESSION['id'];
-        $sql = "SELECT * FROM dosen WHERE user_id = '$id'";
-        $result = mysqli_query($conn, $sql);
-        
+		$sql = "SELECT * FROM dosen WHERE user_id = '$id'";
+		$result = mysqli_query($conn, $sql);
+
 		return $result;
 	}
+}
